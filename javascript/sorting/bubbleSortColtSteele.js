27e23db8d4209a6690c6ae461836223068d72ae6
Frank <frank.mailservices@gmail.com>
2020-01-29 23:28:44 +0530

    colt steele version was updated

--- javascript/sorting/bubbleSortColtSteele.js
+++ javascript/sorting/bubbleSortColtSteele.js
@@ -1,5 +1,16 @@
 function bubbleSort(arr){
-    for(let i = 0;
+    let noswap;
+    for(let i = arr.length; i > 0; i--){
+        noswap = true
+        for(let j = 0; j < i - 1; j++){
+            if( arr[j+1] < arr[j]){
+                [arr[j+1],arr[j]] = [arr[j], arr[j+1]]
+                noswap = false
+            }
+        }
+        if(noswap) break
+    }
+    return arr
 }
 
 console.log(bubbleSort([234,7,2,98,8,37]))
