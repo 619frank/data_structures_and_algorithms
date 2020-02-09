cfca5177f574a42a44d54e5b83001243a8d4ee1a
Frank <frank.mailservices@gmail.com>
2020-02-09 07:56:01 +0530

    added graph

---
+++ javascript/datastructures/graphsAndTraversal/graph.js
@@ -0,0 +1,37 @@
+class Graph{
+    constructor(){
+        this.adjacencyList = {}
+    }
+    //this is the actual value
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+        return true
+    }
+    //this is the connection
+    addEdge(v1,v2){
+        this.adjacencyList[v1].push(v2)
+        this.adjacencyList[v2].push(v1)            
+    }
+
+    removeEdge(vertex1,vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            (v) => v !== vertex2
+        )            
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            (v) => v !== vertex1
+        )            
+    }
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop()
+            this.removeEdge(vertex,adjacentVertex)
+        }
+        delete this.adjacencyList[vertex]
+    }
+}
+console.clear()
+var graph = new Graph
+graph.addVertex('frank')
+graph.addVertex('franky')
+graph.addVertex('alien')
+graph.addEdge('frank','franky')
