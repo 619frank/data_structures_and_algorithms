e00a6c5c1dd25bea8d6f075e88fde963512c4b64
Frank David <frank.david@betterplace.co.in>
2024-06-19 10:52:47 +0530

    Added solution for Leetcode 0234. Palindrome Linked List

--- problems/08. linked_list 2/Leetcode 0234. Palindrome Linked List/java/Solution.java
+++ problems/08. linked_list 2/Leetcode 0234. Palindrome Linked List/java/Solution.java
@@ -1,3 +1,60 @@
-public class Solution {
-    
+class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode() {}
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Solution {
+
+  public boolean isPalindrome(ListNode head) {
+    ListNode secondHalfOfList = getStartOfSecondHalf(head);
+
+    ListNode reversedSecondHalfOfList = reverseLinkedList(secondHalfOfList);
+
+    while (reversedSecondHalfOfList.next != null) {
+      if (reversedSecondHalfOfList.val != head.val) return false;
+
+      reversedSecondHalfOfList = reversedSecondHalfOfList.next;
+      head = head.next;
+    }
+
+    return true;
+  }
+
+  private ListNode getStartOfSecondHalf(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow;
+  }
+
+  private ListNode reverseLinkedList(ListNode head) {
+    ListNode prev = null;
+    ListNode current = head;
+
+    while (current != null) {
+      ListNode next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    return prev;
+  }
 }
