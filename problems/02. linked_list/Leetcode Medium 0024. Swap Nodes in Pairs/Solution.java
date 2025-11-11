c75eda3fca585a7ee8313a1e24f02ddb29f8eb33
619frank <frank.mailservices@gmail.com>
2025-11-11 10:26:11 +0530

    Added solution for Leetcode Medium 0024. Swap Nodes in Pairs

---
+++ problems/02. linked_list/Leetcode Medium 0024. Swap Nodes in Pairs/Solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode point = dummy;
+
+        while(point.next != null && point.next.next !=null){
+            ListNode swap1 = point.next;
+            ListNode swap2 = point.next.next;
+
+            swap1.next = swap2.next;
+            swap2.next = swap1;
+
+            point.next = swap2;
+            point = swap1;
+        }
+        
+
+        return dummy.next;
+    }
+}
